d70fd47055f357a810c69fbe9d57fbe2990d41e3
Florian Kurpicz <florian.kurpicz@tu-dortmund.de>
2019-11-30 21:06:43 +0100

    Compute fingerprints on the fly

--- src/structs/lce_semi_synchronizing_sets.hpp
+++ src/structs/lce_semi_synchronizing_sets.hpp
@@ -1,8 +1,11 @@
+#pragma once
+
 #include "util/lce_interface.hpp"
 #include "util/util.hpp"
 #include "util/synchronizing_sets/bit_vector_rank.hpp"
 #include "util/synchronizing_sets/lce-rmq.hpp"
 
+#include <tlx/math/round_to_power_of_two.hpp>
 
 #include <cstdio>
 #include <string>
@@ -15,259 +18,240 @@
 #include <cmath>
 #include <ctgmath>
 
-
-
 #define unlikely(x)    __builtin_expect(!!(x), 0) 
-#pragma once
+
+template <typename DataType>
+class ring_buffer {
+public:
+  ring_buffer(uint64_t const buffer_size)
+    : buffer_size_(tlx::round_up_to_power_of_two(buffer_size)),
+      mod_mask_(buffer_size_ - 1), size_(0), data_(buffer_size_) { }
+
+  void push_back(DataType const data) {
+    uint64_t const insert_pos = size_++ & mod_mask_;
+    data_[insert_pos] = data;
+  }
+
+  size_t size() const {
+    return size_;
+  }
+
+  DataType operator[](size_t const index) {
+    return data_[index & mod_mask_];
+  }
+
+  DataType operator[](size_t const index) const {
+    return data_[index & mod_mask_];
+  }
+
+private:
+  uint64_t const buffer_size_;
+  uint64_t const mod_mask_;
+
+  uint64_t size_;
+
+  std::vector<DataType> data_;
+
+}; // class ring_buffer
+
 /* This class stores a text as an array of characters and 
  * answers LCE-queries with the naive method. */
 
 class LceSemiSyncSets : public LceDataStructure {
-	public:
-		/* Loads the full file located at PATH. */
-		LceSemiSyncSets(std::string path) {
-			buildStruct(path);
-		}
+public:
+  /* Loads the full file located at PATH. */
+  LceSemiSyncSets(std::string path) {
+    buildStruct(path);
+  }
 		
-		/* Loads a prefix of the file located at PATH */ 
-		LceSemiSyncSets(std::string path, uint64_t number_of_chars) {
-		}
+  /* Loads a prefix of the file located at PATH */ 
+  LceSemiSyncSets(std::string path, uint64_t number_of_chars) {
+  }
 		
-		~LceSemiSyncSets() {
-		}
+  ~LceSemiSyncSets() {
+  }
 		
-		/* Answers the lce query for position i and j */
-		inline uint64_t lce(const uint64_t i, const uint64_t j) {
+  /* Answers the lce query for position i and j */
+  inline uint64_t lce(const uint64_t i, const uint64_t j) {
 			
-			timer.reset();
-			if (i==j) {
-				return text_length_in_bytes_ - i;
-			}
-			/* naive part */
-			for(unsigned int k = 0; k < (3*kTau-1); ++k) {
-				if(text_[i+k] != text_[j+k]) {
-					return k;
-				}
-			}
-			//~ const uint64_t max_length = text_length_in_bytes_ - ((i < j) ? j : i);
-			//~ uint64_t lce = 0;
-			//~ const unsigned __int128 * text_blocks_i = (unsigned __int128*) (text_.c_str() + i);
-			//~ const unsigned __int128 * text_blocks_j = (unsigned __int128*) (text_.c_str() + j);
+    timer.reset();
+    if (i==j) {
+      return text_length_in_bytes_ - i;
+    }
+    /* naive part */
+    for(unsigned int k = 0; k < (3*kTau-1); ++k) {
+      if(text_[i+k] != text_[j+k]) {
+        return k;
+      }
+    }
 			
-			//~ if(max_length <= 3*kTau) {
-				//~ // Accelerate search by comparing 16-byte blocks
-				
-				//~ for(; lce < max_length/16; ++lce) {
-					//~ if(text_blocks_i[lce] != text_blocks_j[lce]) {
-						//~ break;
-					//~ }
-				//~ }
-				//~ lce *= 16;
-				//~ // The last block did not match. Here we compare its single characters
-				//~ uint64_t lce_end = lce + ((16 < max_length) ? 16 : max_length);
-				//~ for (; lce < lce_end; ++lce) {
-					//~ if(text_[i + lce] != text_[j + lce]) {
-						//~ break;
-					//~ }
-				//~ }
-				//~ return lce;
-			//~ }
+    ts_naive += timer.elapsed();
 			
+    timer.reset();
+    /* strSync part */
+    uint64_t i_ = suc(i);
+    uint64_t j_ = suc(j);
+    ts_rank += timer.elapsed();
 			
-			//~ // Accelerate search by comparing 16-byte blocks
-			//~ //util::printInt128(text_blocks_i[0]);
-			//~ //std::cout << text_blocks_j << std::endl;
-			//~ for(; lce < 1024/16*3; ++lce) {
-				//~ //std::cout << "lce" << lce << std::endl;
-				//~ if(text_blocks_i[lce] != text_blocks_j[lce]) {
-					//~ break;
-				//~ }
-			//~ }
-			//~ lce *= 16;
-			//~ // The last block did not match. Here we compare its single characters
-			//~ for (; lce < 1024; ++lce) {
-				//~ if(text_[i + lce] != text_[j + lce]) {
-					//~ return lce;
-				//~ }
-			//~ }
+    timer.reset();
+    uint64_t l = lce_rmq_->lce(i_, j_);
+    ts_rmq_lce += timer.elapsed();
 			
-			ts_naive += timer.elapsed();
-			
-			timer.reset();
-			/* strSync part */
-			uint64_t i_ = suc(i);
-			uint64_t j_ = suc(j);
-			ts_rank += timer.elapsed();
-			
-			timer.reset();
-			uint64_t l = lce_rmq_->lce(i_, j_);
-			ts_rmq_lce += timer.elapsed();
-			
-			return l + s_[i_] - i;
-		}
+    return l + sync_set_[i_] - i;
+  }
 		
-		char operator[](uint64_t i) {
-			if(i > text_length_in_bytes_) {return '\00';}
-			return text_[i];
-		}
+  char operator[](uint64_t i) {
+    if(i > text_length_in_bytes_) {return '\00';}
+    return text_[i];
+  }
 		
-		int isSmallerSuffix(const uint64_t i, const uint64_t j) {
-			return 0;
-		}
+  int isSmallerSuffix(const uint64_t i, const uint64_t j) {
+    return 0;
+  }
 		
-		size_t getSizeInBytes() {
-			return text_length_in_bytes_;
-		}
+  size_t getSizeInBytes() {
+    return text_length_in_bytes_;
+  }
 		
 		
-		double getTimeNaive() {
-			return ts_naive;
-		}
-		double getTimeRank() {
-			return ts_rank;
-		}
-		double getTimeRmq() {
-			return ts_rmq_lce;
-		}
-		void resetTimer() {
-			ts_naive = 0.0;
-			ts_rank = 0.0;
-			ts_rmq_lce = 0.0;
-		}
+  double getTimeNaive() {
+    return ts_naive;
+  }
+  double getTimeRank() {
+    return ts_rank;
+  }
+  double getTimeRmq() {
+    return ts_rmq_lce;
+  }
+  void resetTimer() {
+    ts_naive = 0.0;
+    ts_rank = 0.0;
+    ts_rmq_lce = 0.0;
+  }
 		
-	private:
-		std::string text_;
-		size_t text_length_in_bytes_;
+private:
+  std::string text_;
+  size_t text_length_in_bytes_;
 		
-		const unsigned __int128 kPrime = 18446744073709551557ULL;
-		const uint64_t kTau = 1024;
-		uint64_t two_pow_tau_mod_q_;
+  const unsigned __int128 kPrime = 18446744073709551557ULL;
+  const uint64_t kTau = 1024;
+  uint64_t two_pow_tau_mod_q_;
 
 		
-		std::vector<uint64_t> s_;
-		bit_vector * s_bv_;
-		bit_vector_rank * s_bvr_;
+  std::vector<uint64_t> s_;
+  std::vector<uint64_t> sync_set_;
+  bit_vector * s_bv_;
+  bit_vector_rank * s_bvr_;
 		
-		Lce_rmq * lce_rmq_;
+  Lce_rmq * lce_rmq_;
 		
-		double ts_naive, ts_rank, ts_rmq_lce;
-		util::Timer timer;
+  double ts_naive, ts_rank, ts_rmq_lce;
+  util::Timer timer;
 
-		/* Finds the smallest element that is greater or equal to i
-		Because s_ is ordered, that is equal to the 
-		first element greater than i */
-		inline uint64_t suc(uint64_t i) const {
-			return s_bvr_->rank1(i);
-		}
-		
-		
-		void fillS(const uint64_t from, const uint64_t to, const vector<uint64_t> &fingerprints) {
-			
-			uint64_t min;
-			for (uint64_t i = from; i < to; ) {
-				// Compare this id with every other index which is not in q
-				min = 0;
-				for (unsigned int j = 1; j <= kTau; ++j) {
-					if(fingerprints[i+j] < fingerprints[i+min]) {
-						min = j;
-					}
-				}
-				if(min == 0 || min == kTau) {
-					s_.push_back(i);
-				}
+  /* Finds the smallest element that is greater or equal to i
+     Because s_ is ordered, that is equal to the 
+     first element greater than i */
+  inline uint64_t suc(uint64_t i) const {
+    return s_bvr_->rank1(i);
+  }
+
+  void fill_synchronizing_set(const uint64_t from, const uint64_t to,
+                              unsigned __int128& fp,
+                              ring_buffer<uint64_t>& fingerprints) {
+
+    uint64_t min;
+    for (uint64_t i = from; i < to; ) {
+      // Compare this id with every other index which is not in q
+      min = 0;
+      size_t required = i + kTau - (fingerprints.size() - 1);
+      calculate_fingerprints(required, fp, fingerprints);
+      for (unsigned int j = 1; j <= kTau; ++j) {
+        if(fingerprints[i+j] < fingerprints[i+min]) {
+          min = j;
+        }
+      }
+      if(min == 0 || min == kTau) {
+        sync_set_.push_back(i);
+      }
 				
 				
-				uint64_t local_min = i + min;
-				++i;
-				while(i < to && i < local_min) {
-					if(fingerprints[i+kTau] <= fingerprints[local_min]) {
-						s_.push_back(i);
-						local_min = i + kTau; 
-					}
-					i++;
-				}
-			}
-		}
-		
-		
-		void buildStruct(std::string path) {
-			std::ifstream input(path);
-			input.seekg(0);
-			util::inputErrorHandling(&input);
-			std::stringstream buffer;
-			buffer << input.rdbuf();
-			text_ = buffer.str();
-			text_length_in_bytes_ = text_.size();
-			std::cout << "T size: " << text_.size() << std::endl;
-			
-			// Timer for benchmark
-			util::Timer timer{};
+      uint64_t local_min = i + min;
+      ++i;
+      calculate_fingerprints(1, fp, fingerprints);
+      while(i < to && i < local_min) {
+        if(fingerprints[i+kTau] <= fingerprints[local_min]) {
+          sync_set_.push_back(i);
+          local_min = i + kTau; 
+        }
+        i++;
+        calculate_fingerprints(1, fp, fingerprints);
+      }
+    }
+  }
 
-			// Calculate fingerprints
-			std::cout << "Calculating FP" << std::endl;
-			unsigned __int128 fp = 0;
-			for(uint64_t i = 0; i < kTau; ++i) {
-				fp *= 256;
-				fp += (unsigned char) text_[i];
-				fp %= kPrime;
-			}
-			std::vector<uint64_t> t_fp;
-			t_fp.push_back((uint64_t) fp);
-			two_pow_tau_mod_q_ = calculatePowerModulo(10);
-			for(uint64_t i = 0; i < text_length_in_bytes_-kTau; ++i) {
-				fp *= 256;
-				fp += (unsigned char) text_[kTau+i];
-				fp %= kPrime;
+  void calculate_fingerprints(size_t const count, unsigned __int128& fp,
+                              ring_buffer<uint64_t>& fingerprints) {
+    for(uint64_t i = 0; i < count; ++i) {
+      fp *= 256;
+      fp += (unsigned char) text_[kTau+fingerprints.size() - 1];
+      fp %= kPrime;
 				
-				unsigned __int128 first_char_influence = text_[i];
-				first_char_influence *= two_pow_tau_mod_q_;
-				first_char_influence %= kPrime;
+      unsigned __int128 first_char_influence = text_[fingerprints.size() - 1];
+      first_char_influence *= two_pow_tau_mod_q_;
+      first_char_influence %= kPrime;
 				
-				if(first_char_influence < fp) {
-					fp -= first_char_influence;
-				} else {
-					fp = kPrime - (first_char_influence - fp);
-				}
-				t_fp.push_back((uint64_t) fp);
-			}
-			std::cout << "FP calculated" << std::endl;
-			std::cout << "FP Size: " << t_fp.size() << std::endl;
-
-
+      if(first_char_influence < fp) {
+        fp -= first_char_influence;
+      } else {
+        fp = kPrime - (first_char_influence - fp);
+      }
+      fingerprints.push_back(static_cast<uint64_t>(fp));
+    }
+  }
 		
-			//Calculate S
-			fillS(0, (text_length_in_bytes_ - (2*kTau)), t_fp);
-			
-			// LOAD S FROM A FILE
-			//~ std::ifstream sLoad("../res/ecoli.fa", std::ios::in);
-			//~ for (std::string line; std::getline(sLoad, line); ) {
-				//~ s_.push_back(stoi(line));
-			//~ }
-			
-			std::cout << "S size: " << s_.size() << std::endl;
-			s_.shrink_to_fit();
-			
-			// SAVE S IN A FILE
-			//~ std::ofstream s_set("../res/ecoli.fa", std::ios::out|std::ios::trunc);
-			//~ for(uint64_t i = 0; i < s_.size(); ++i) {
-				//~ s_set << s_[i] << std::endl;
-			//~ }
-			
-			s_bv_ = new bit_vector(text_length_in_bytes_);
-			for(size_t i = 0; i < s_.size(); ++i) {
-				s_bv_->bitset(s_[i], 1);
-			}
-			
-			s_bvr_ = new bit_vector_rank(*s_bv_);
-			lce_rmq_ = new Lce_rmq(&text_, &s_);
-		}
 		
-		uint64_t calculatePowerModulo(unsigned int power) {
-				//powerTable = new uint64_t[numberOfLevels];
-				unsigned __int128 x = 256;
-				for (unsigned int i = 0; i < power; i++) {
-					x = (x*x) % kPrime;
-				}
-				return (uint64_t) x;
-			}
+  void buildStruct(std::string path) {
+    std::ifstream input(path);
+    input.seekg(0);
+    util::inputErrorHandling(&input);
+    std::stringstream buffer;
+    buffer << input.rdbuf();
+    text_ = buffer.str();
+    text_length_in_bytes_ = text_.size();
+    std::cout << "T size: " << text_.size() << std::endl;
+			
+    // Timer for benchmark
+    util::Timer timer{};
+
+    // Calculate fingerprints
+    std::cout << "Calculating FP" << std::endl;
+    unsigned __int128 fp = 0;
+    for(uint64_t i = 0; i < kTau; ++i) {
+      fp *= 256;
+      fp += (unsigned char) text_[i];
+      fp %= kPrime;
+    }
+    ring_buffer<uint64_t> fingerprints(3*kTau);
+    fingerprints.push_back(static_cast<uint64_t>(fp));
+    two_pow_tau_mod_q_ = calculatePowerModulo(10);
+    fill_synchronizing_set(0, (text_length_in_bytes_ - (2*kTau)), fp, fingerprints);
+    sync_set_.shrink_to_fit();
+			
+    s_bv_ = new bit_vector(text_length_in_bytes_);
+    for(size_t i = 0; i < sync_set_.size(); ++i) {
+      s_bv_->bitset(sync_set_[i], 1);
+    }
+			
+    s_bvr_ = new bit_vector_rank(*s_bv_);
+    lce_rmq_ = new Lce_rmq(&text_, &sync_set_);
+  }
+		
+  uint64_t calculatePowerModulo(unsigned int power) {
+    //powerTable = new uint64_t[numberOfLevels];
+    unsigned __int128 x = 256;
+    for (unsigned int i = 0; i < power; i++) {
+      x = (x*x) % kPrime;
+    }
+    return (uint64_t) x;
+  }
 };
