9f23eed68991eb4a802c47092b9b4ad574e93cb6
Alexander Herlez <alexander.herlez@tu-dortmund.de>
2021-08-16 15:32:54 +0200

    fix errors

--- lce-test/util_ssss_par/ssss_par.hpp
+++ lce-test/util_ssss_par/ssss_par.hpp
@@ -24,22 +24,24 @@ class string_synchronizing_set_par {
     std::vector<std::vector<t_index>> sss_part{};
     sss_part.resize(omp_get_max_threads());
 
-    #pragma omp parallel
+#pragma omp parallel
     {
-      const size_t size_per_thread = text.size() / omp_get_num_threads();
+      const size_t sss_end = text.size() - 2 * t_tau + 1;
+      const size_t size_per_thread = (sss_end / omp_get_num_threads()) + 1;
       const int t = omp_get_thread_num();
-      sss_part[t] = fill_synchronizing_set(text, size_per_thread * t, std::min(text.size(), size_per_thread * (t + 1)));
+      sss_part[t] = fill_synchronizing_set(text, size_per_thread * t, std::min(sss_end, size_per_thread * (t + 1)));
     }
 
     //Merge parts
+
     std::vector<size_t> write_pos{0};
     for (auto& part : sss_part) {
       write_pos.push_back(write_pos.back() + part.size());
     }
     size_t sss_size = write_pos.back();
-    m_sss.resize(sss_size);
 
-    #pragma omp parallel
+    m_sss.resize(sss_size);
+#pragma omp parallel
     {
       const int t = omp_get_thread_num();
       std::copy(sss_part[t].begin(), sss_part[t].end(), m_sss.begin() + write_pos[t]);
@@ -50,43 +52,50 @@ class string_synchronizing_set_par {
     std::vector<t_index> sss;
 
     //Calculate first fingerprint
-    herlez::rolling_hash::rk_prime<decltype(text.cbegin()), 107> rk(text.cbegin(), t_tau);
-    
+    herlez::rolling_hash::rk_prime<decltype(text.cbegin()), 107> rk(text.cbegin() + from, t_tau, 296813);
+
     uint128_t fp = rk.get_currect_fp();
 
     ring_buffer<uint128_t> fingerprints(4 * t_tau);
     fingerprints.resize(from);
     fingerprints.push_back(fp);
 
-    uint128_t min = 0;
     for (size_t i = from; i < to;) {
       // Compare this id with every other index which is not in q
-      min = 0;
       size_t required = i + t_tau - (fingerprints.size() - 1);
-      for(size_t num_rolled = 0; num_rolled < required; ++num_rolled) {
+      for (size_t num_rolled = 0; num_rolled < required; ++num_rolled) {
         fingerprints.push_back(rk.roll());
       }
+      uint128_t first_min = i;
+      bool add_index = true;
 
-      for (unsigned int j = 1; j <= t_tau; ++j) {
-        if (fingerprints[i + j] < fingerprints[i + min]) {
-          min = j;
+      for (unsigned int j = i + 1; j < i + t_tau; ++j) {
+        if (fingerprints[j] < fingerprints[first_min]) {
+          add_index = false;
+          first_min = j;
         }
       }
-      if (min == 0 || min == t_tau) {
+      if (fingerprints[i + t_tau] <= fingerprints[first_min]) {
+        add_index = true;
+        if (fingerprints[i + t_tau] != fingerprints[first_min]) {
+          first_min = i + t_tau;
+        }
+      }
+
+      if (add_index) {
         sss.push_back(i);
       }
 
-      size_t local_min = i + min;
       ++i;
       fingerprints.push_back(rk.roll());
-      while (i < to && i < local_min) {
-        if (fingerprints[i + t_tau] <= fingerprints[local_min]) {
+      while (i < to && i < first_min) {
+        if (fingerprints[i + t_tau] <= fingerprints[first_min]) {
           sss.push_back(i);
-          if (fingerprints[i + t_tau] != fingerprints[local_min]) {
-            local_min = i + t_tau;
+          if (fingerprints[i + t_tau] != fingerprints[first_min]) {
+            first_min = i + t_tau;
           }
         }
-        i++;
+        ++i;
         fingerprints.push_back(rk.roll());
       }
     }
