30db595df51398fcba9897d20a0cea8406e5363d
Alexander Herlez <alexander.herlez@tu-dortmund.de>
2021-08-23 16:31:08 +0200

    simplify paralll sss construction and add run-detecting version

--- lce-test/util_ssss_par/ssss_par.hpp
+++ lce-test/util_ssss_par/ssss_par.hpp
@@ -14,6 +14,7 @@ class string_synchronizing_set_par {
 
  private:
   std::vector<t_index> m_sss;
+  bool m_runs_detected;
 
  public:
   std::vector<t_index> get_sss() {
@@ -21,24 +22,43 @@ class string_synchronizing_set_par {
   }
 
   string_synchronizing_set_par(const std::vector<uint8_t>& text) {
-    std::vector<std::vector<t_index>> sss_part{};
-    sss_part.resize(omp_get_max_threads());
+    std::vector<std::vector<t_index>> sss_part(omp_get_max_threads());
 
 #pragma omp parallel
     {
       const size_t sss_end = text.size() - 2 * t_tau + 1;
       const size_t size_per_thread = (sss_end / omp_get_num_threads()) + 1;
       const int t = omp_get_thread_num();
-      sss_part[t] = fill_synchronizing_set(text, size_per_thread * t, std::min(sss_end, size_per_thread * (t + 1)));
+      const size_t start = size_per_thread * t;
+      const size_t end = (t == omp_get_num_threads() - 1) ? sss_end : (t + 1) * size_per_thread;
+      sss_part[t] = fill_synchronizing_set(text, start, end);
     }
 
-    //Merge parts
-
+    //Merge SSS parts
     std::vector<size_t> write_pos{0};
     for (auto& part : sss_part) {
       write_pos.push_back(write_pos.back() + part.size());
     }
-    size_t sss_size = write_pos.back();
+    size_t sss_size = write_pos.back();  //+1 for sentinel
+    m_runs_detected = sss_size > text.size()*6 / t_tau;
+
+    //If the text contains long runs, the sss inflates. We the then use a algorithm which detects runs.
+    if (m_runs_detected) {
+#pragma omp parallel
+      {
+        const size_t sss_end = text.size() - 2 * t_tau + 1;
+        const size_t size_per_thread = (sss_end / omp_get_num_threads()) + 1;
+        const int t = omp_get_thread_num();
+        const size_t start = size_per_thread * t;
+        const size_t end = (t == omp_get_num_threads() - 1) ? sss_end : (t + 1) * size_per_thread;
+        sss_part[t] = fill_synchronizing_set_runs(text, start, end);
+      }
+      write_pos = {0};
+      for (auto& part : sss_part) {
+        write_pos.push_back(write_pos.back() + part.size());
+      }
+      sss_size = write_pos.back() + 1;  //+1 for sentinel
+    }
 
     m_sss.resize(sss_size);
 #pragma omp parallel
@@ -46,59 +66,193 @@ class string_synchronizing_set_par {
       const int t = omp_get_thread_num();
       std::copy(sss_part[t].begin(), sss_part[t].end(), m_sss.begin() + write_pos[t]);
     }
+    if (m_runs_detected) {
+      m_sss.back() = text.size() - 2 * t_tau + 1;  //sentinel needed for text with runs
+    }
   }
 
   std::vector<t_index> fill_synchronizing_set(const std::vector<uint8_t>& text, const size_t from, const size_t to) const {
+    //calculate SSS
     std::vector<t_index> sss;
 
-    //Calculate first fingerprint
     herlez::rolling_hash::rk_prime<decltype(text.cbegin()), 107> rk(text.cbegin() + from, t_tau, 296813);
-
-    uint128_t fp = rk.get_currect_fp();
-
     ring_buffer<uint128_t> fingerprints(4 * t_tau);
     fingerprints.resize(from);
-    fingerprints.push_back(fp);
+    fingerprints.push_back(rk.get_current_fp());
 
-    for (size_t i = from; i < to;) {
-      // Compare this id with every other index which is not in q
-      size_t required = i + t_tau - (fingerprints.size() - 1);
-      for (size_t num_rolled = 0; num_rolled < required; ++num_rolled) {
+    t_index first_min = 0;
+
+    //Loop:
+    for (size_t i = from; i < to; ++i) {
+      for (size_t j = fingerprints.size(); j <= i + t_tau; ++j) {
         fingerprints.push_back(rk.roll());
       }
-      uint128_t first_min = i;
-      bool add_index = true;
 
-      for (unsigned int j = i + 1; j < i + t_tau; ++j) {
-        if (fingerprints[j] < fingerprints[first_min]) {
-          add_index = false;
-          first_min = j;
-        }
-      }
-      if (fingerprints[i + t_tau] <= fingerprints[first_min]) {
-        add_index = true;
-        if (fingerprints[i + t_tau] != fingerprints[first_min]) {
-          first_min = i + t_tau;
-        }
-      }
-
-      if (add_index) {
-        sss.push_back(i);
-      }
-
-      ++i;
-      fingerprints.push_back(rk.roll());
-      while (i < to && i < first_min) {
-        if (fingerprints[i + t_tau] <= fingerprints[first_min]) {
-          sss.push_back(i);
-          if (fingerprints[i + t_tau] != fingerprints[first_min]) {
-            first_min = i + t_tau;
+      //if (min == 0) || min < i {min\Q = calculate_min(); if(min > i+ t_tau { update(i), update(min)})} else
+      if (first_min == 0 || first_min < i) {
+        first_min = i;
+        for (size_t j = i; j <= i + t_tau; ++j) {
+          //compare values that are not in q
+          if (fingerprints[j] < fingerprints[first_min]) {
+            first_min = j;
           }
         }
-        ++i;
-        fingerprints.push_back(rk.roll());
+      }
+      //if min <= i+t_tau {if(i+t_tau not in q) maybe_update(min); } else
+      else if (fingerprints[i + t_tau] < fingerprints[first_min]) {
+        first_min = i + t_tau;
+      }
+
+      if (fingerprints[first_min] == fingerprints[i] || fingerprints[first_min] == fingerprints[i + t_tau]) {
+        sss.push_back(i);
       }
     }
     return sss;
+  }
+
+  std::vector<t_index> fill_synchronizing_set_runs(const std::vector<uint8_t>& text, const size_t from, const size_t to) const {
+    //calculate Q
+    std::vector<std::pair<t_index, t_index>> qset = calculate_q(text, from, to);
+    
+    /*
+    #pragma omp critical
+    {
+      std::cout << "from " << from << " to " << to << '\n';
+      //std::cout << "qset = { ";
+      std::cout << qset.size() << '\n';
+      //for (auto n : qset) {
+      //  std::cout << "<" << n.first << ", " << n.second << "> ";
+      //}
+      //std::cout << "}; \n";
+    }
+    */
+    
+    qset.push_back(std::make_pair(std::numeric_limits<t_index>::max(), std::numeric_limits<t_index>::max()));
+    auto it_q = qset.begin();
+    //calculate SSS
+    //BEGIN
+    std::vector<t_index> sss;
+
+    herlez::rolling_hash::rk_prime<decltype(text.cbegin()), 107> rk(text.cbegin() + from, t_tau, 296813);
+    ring_buffer<uint128_t> fingerprints(4 * t_tau);
+    fingerprints.resize(from);
+    fingerprints.push_back(rk.get_current_fp());
+
+    t_index first_min = 0;
+
+    //Loop:
+    for (size_t i = from; i < to; ++i) {
+      //get_fingerprints_until(i+t_tau);
+      for (size_t j = fingerprints.size(); j <= i + t_tau; ++j) {
+        fingerprints.push_back(rk.roll());
+      }
+      while (it_q->second < i) {
+        std::advance(it_q, 1);
+      }
+
+      //if (min == 0) || min < i {min\Q = calculate_min(); if(min > i+ t_tau { update(i), update(min)})} else
+      if (first_min == 0 || first_min < i) {
+        auto it_qt = it_q;
+        for (size_t j = i; j <= i + t_tau; ++j) {
+          //advance q pointer
+          if (it_qt->second < j) {
+            std::advance(it_qt, 1);
+          }
+          //don't compare values from q
+          if (it_qt->first <= j) {
+            first_min = it_qt->second + 1;
+            j = first_min;
+            continue;
+          }
+          //take first fingerprint not in q
+          if (first_min == 0 || first_min < i) {
+            first_min = j;
+          }
+          //compare values that are not in q
+          if (fingerprints[j] < fingerprints[first_min]) {
+            first_min = j;
+          }
+        }
+        if (first_min > i + t_tau) {
+          i = first_min - t_tau;
+          --i;  //balance
+          continue;
+        }
+      }
+      //if min <= i+t_tau {if(i+t_tau not in q) maybe_update(min); } else
+      else if (first_min <= i + t_tau) {
+        auto it_qt = it_q;
+        while (it_qt->second < i + t_tau) {
+          std::advance(it_qt, 1);
+        }
+        if (it_qt->first > i + t_tau && fingerprints[i + t_tau] < fingerprints[first_min]) {
+          first_min = i + t_tau;
+        }
+      }
+      //maybe_add(i);
+      if (fingerprints[first_min] == fingerprints[i] || fingerprints[first_min] == fingerprints[i + t_tau]) {
+        sss.push_back(i);
+      }
+    }
+    return sss;
+  }
+
+  std::vector<std::pair<t_index, t_index>> calculate_q(const std::vector<uint8_t>& text, const size_t from, const size_t to) const {
+    std::vector<std::pair<t_index, t_index>> qset{};
+    constexpr size_t small_tau = t_tau / 3;
+    herlez::rolling_hash::rk_prime<decltype(text.cbegin()), 107> rk(text.cbegin() + from, small_tau, 296813);
+
+    ring_buffer<uint128_t> fingerprints(4 * t_tau);
+    fingerprints.resize(from);
+    fingerprints.push_back(rk.get_current_fp());
+
+    for (size_t i = from; i < to + t_tau; ++i) {  //++i correct?
+      for (size_t j = fingerprints.size(); j < i + t_tau; ++j) {
+        fingerprints.push_back(rk.roll());
+      }
+      //find first minimum
+      size_t first_min = i;
+      for (size_t j = i; j < i + small_tau; ++j) {
+        if (fingerprints[j] < fingerprints[first_min]) {
+          first_min = j;
+        }
+      }
+      //look for next (same) minimum
+      size_t next_min = first_min;
+      for (size_t j = first_min + 1; j < first_min + small_tau; ++j) {
+        if (fingerprints[j] == fingerprints[first_min]) {
+          next_min = j;
+          break;
+        }
+      }
+
+      if (next_min == first_min) {
+        //if no matching fingerprint exists, start again after the min
+        i = first_min;
+      } else {
+        //if matching fingerprint exists, extend the run and add it to q
+        size_t const period = next_min - first_min;
+        //now extend run naivly to the left
+        size_t run_start = first_min;
+        while (run_start != from && text[run_start] == text[run_start + period]) {
+          --run_start;
+        }
+        if (run_start != from || text[0] != text[0 + period]) {
+          ++run_start;
+        }
+        //extend run naivly to the right
+        size_t run_end = next_min;
+        while (run_end < to + 2 * t_tau - 1 && text[run_end] == text[run_end - period]) {
+          ++run_end;
+        }
+        --run_end;
+
+        if (run_end - run_start + 1 > t_tau) {
+          qset.push_back(std::make_pair(run_start, run_end - t_tau + 1));
+          i = run_end - small_tau;  //TODO: too tight?
+        }
+      }
+    }
+    return qset;
   }
 };
